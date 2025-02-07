d89b360d12953ae3a52283fca6450ec19613d4d2
SemzyTheMan <semzy2002@gmail.com>
2025-02-07 10:09:07 +0100

    feat:doneee

--- src/main/java/com/semzy_ecommerce/soft/service/UserService.java
+++ src/main/java/com/semzy_ecommerce/soft/service/UserService.java
@@ -10,12 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService  {
-
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    private UserRepository userRepository;
-
-
-    private PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Transactional
     public void registerUser(User user) {
